e524cb9be6e25c4552c2a124501423a060990055
Willian Oliviera <willi12345677@gmail.com>
2025-10-05 14:40:44 -0300

    primeiro comentario

---
+++ src/main/java/com/willianoliveira/projetobeta/ProjetobetaApplication.java
@@ -0,0 +1,13 @@
+package com.willianoliveira.projetobeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetobetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetobetaApplication.class, args);
+	}
+
+}
